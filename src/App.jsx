4d25b05363d78f077c4e03fef72b1c426631f8d6
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-20 19:14:23 -0300

    Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
 import Profile from './components/Profile'
+import Links from './components/Links'
 
 function App() {
 
   return (
     <div>
     <Profile/>
+    <Links/>
     </div>
   )
 }
---
+++ src/components/Links/index.jsx
@@ -0,0 +1,23 @@
+import './Links.css'
+
+const Links = () => {
+    const listaRedes = [
+        {id:0, nome:'GitHub', urlRede:'https://github.com/smkhb'},
+        {id:1, nome:'LinkedIn', urlRede:'https://www.linkedin.com/in/samuelbernardo/'},
+        {id:2, nome:'Twitter', urlRede:'https://twitter.com/Smk_hb' },
+        {id:3, nome:'Instagram', urlRede: 'https://www.instagram.com/samuka_hb/'},
+        {id:4, nome:'Vercel', urlRede: 'https://vercel.com/smkhb'}
+    ]
+    
+    const LinksRedes = listaRedes.map(props => 
+        <div key={props.id} className='Lista-Links'>
+            <a href={props.urlRede}>{props.nome}</a>
+        </div>
+    )
+    return (
+        <>
+            {LinksRedes}
+        </>
+    )
+}
+export default Links
