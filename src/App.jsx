961a1fabe457ae5364febd26cfb1376d39e89bdc
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-20 16:41:08 -0300

    Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Profile from './components/Profile'
 
 function App() {
 
   return (
     <div>
-    <h1>Hello</h1>
+    <Profile/>
     </div>
   )
 }
---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,11 @@
+import './Profile.css'
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <img src='https://www.github.com/smkhb.png' alt='Foto de perfil github' />
+            <h1 className='nome'>Samuel Bernardo</h1>
+            <h3>Desenvolvedor Front-End Jr</h3>
+        </div>
+    )
+}
+export default Profile
